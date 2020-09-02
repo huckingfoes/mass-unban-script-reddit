fd8771b7c2e3b0960dba349d48f4c3b65c988656
Henry Dixon <henry@hdixon.xyz>
2020-09-02 16:48:18 -0400

    release

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # mass-unban-script
 
 Allows moderators to mass unban users between a date range.
+
+1. pip install -r requirements.txt
+2. Replace values in praw-example.ini with your API information
+3. Rename praw-example.ini to praw.ini
+4. Edit the constants on the top of mass-unbanner.py to specify dates and subreddit
+5. Run the script: python3 mass-unbanner.py
+6. You'll see all the users print out that are within the range you specified, to be unbanned
+7. If this is correct, un-comment (remove #) on line 25 to enable ban removal behavior
+8. Congrats. You've just unbanned thousands of users.
---
+++ mass-unbanner.py
@@ -0,0 +1,27 @@
+import praw
+from datetime import datetime
+from dateutil.parser import parse
+
+# EDIT THESE
+START_BAN_PERIOD = "YYYY-MM-DD" # date to start unbanning
+END_BAN_PERIOD = "YYYY-MM-DD" # date to stop unbanning
+SUBREDDIT_NAME = "SUBREDDIT" # subreddit you want to mass-unban;
+
+botName = "mass-unban-bot"
+botDescription = "A mass unbanning script by u/huckingfoes"
+
+reddit = praw.Reddit(botName, user_agent=botDescription)
+
+startDate = parse(START_BAN_PERIOD)
+endDate = parse(END_BAN_PERIOD)
+
+subreddit = reddit.subreddit(SUBREDDIT_NAME)
+
+for bannedUser in subreddit.banned():
+	unixTimestamp = int(bannedUser.date)
+	currentBanDate = datetime.utcfromtimestamp(unixTimestamp)
+	
+	if (currentBanDate > startDate) and (currentBanDate < endDate): 
+#		subreddit.banned.remove(bannedUser)
+		print('Unbanned: ' + '{}: {}'.format(bannedUser, bannedUser.note))
+	
